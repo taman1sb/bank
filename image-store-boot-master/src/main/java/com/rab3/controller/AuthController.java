8917faddd886bb0f057f04db37617d43a0b6d648
taman1sb <64706279+taman1sb@users.noreply.github.com>
2021-01-31 17:39:58 -0600

    5th

--- image-store-boot-master/src/main/java/com/rab3/controller/AuthController.java
+++ image-store-boot-master/src/main/java/com/rab3/controller/AuthController.java
@@ -43,7 +43,7 @@ public class AuthController {
 				model.addAttribute("magic", profileDTO);
 				return "home";  //     /home.jsp
 			}else {
-				model.addAttribute("msg", "Sorry!! username and password are nt valid!!!!!!!!!!!!!!!!!!!!!!!");
+				model.addAttribute("msg", "Sorry!! username v and password are nt valid!!!!!!!!!!!!!!!!!!!!!!!");
 				return "login";
 			}
 	}
